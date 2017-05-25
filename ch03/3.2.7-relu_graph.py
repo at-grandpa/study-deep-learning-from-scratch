e9dccd075bb904ac31c4fa12bc6f056d70c375ce
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2017-05-25 13:20:41 +0900

    ~ 3.3.3

--- ch03/3.2.7-relu_graph.py
+++ ch03/3.2.7-relu_graph.py
@@ -1,11 +1,11 @@
 from lib import common as c
 
-def sigmoid(x):
-    return 1 / (1 + c.np.exp(-x))
+def relu(x):
+    return c.np.maximum(0, x)
 
 x = c.np.arange(-5.0, 5.0, 0.1)
-y = sigmoid(x)
+y = relu(x)
 c.plt.plot(x, y)
-c.plt.ylim(-0.1, 1.1)
+c.plt.ylim(-0.1, 5.1)
 c.plt_show_alt(c.plt)
 
