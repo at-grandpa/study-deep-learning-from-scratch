36e3f78581bbc93245cc5bd2b6b7763c22b609bf
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2017-06-19 21:09:20 +0900

    add image_show_alt

--- lib/common.py
+++ lib/common.py
@@ -6,8 +6,14 @@ import numpy as np
 import matplotlib as mpl
 mpl.use('Agg')
 from matplotlib import pyplot as plt
+from PIL import Image
 
 def plt_show_alt(plt):
     plt.savefig("/tmp/output.png")
     process = Popen(["/usr/local/bin/imgcat", "/tmp/output.png"])
     plt.clf()
+
+def image_show_alt(img):
+    pil_img = Image.fromarray(np.uint8(img))
+    pil_img.save("/tmp/output.png")
+    process = Popen(["/usr/local/bin/imgcat", "/tmp/output.png"])
