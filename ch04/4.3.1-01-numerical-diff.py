98ab6b457dc3866393b23b648f37db7ca5c64cd7
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2017-07-19 13:06:51 +0900

    fix 4.3.1

--- ch04/4.3.1-01-numerical-diff.py
+++ ch04/4.3.1-01-numerical-diff.py
@@ -11,13 +11,23 @@ def numerical_diff(f, x) :
 def function_1(x) :
     return 0.01*x**2 + 0.1*x
 
+def tangent_line(f, x):
+    d = numerical_diff(f, x)
+    print(d)
+    y = f(x) - d*x
+    return lambda t: d*t + y
+
 x = c.np.arange(0.0, 20.0, 0.1)
 y = function_1(x)
 
 c.plt.xlabel("x")
 c.plt.ylabel("f(x)")
 
+tf = tangent_line(function_1, 5)
+y2 = tf(x)
+
 c.plt.plot(x, y)
+c.plt.plot(x, y2)
 c.plt_show_alt(c.plt)
 
 print(numerical_diff(function_1, 5))
